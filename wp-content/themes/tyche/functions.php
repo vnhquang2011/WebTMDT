1f3b2394118d80e23a3dd322ed3710ae8c5e8af7
Quang Truong <1513772@hcmut.edu.vn>
2018-10-09 15:19:19 +0700

    Add So Luong before Them vao gio

--- wp-content/themes/tyche/functions.php
+++ wp-content/themes/tyche/functions.php
@@ -12,3 +12,15 @@
  */
 require_once 'inc/class-tyche-autoloader.php';
 $tyche = new Tyche();
+
+/**
+ * @snippet       Add "So Luong" Label in front of Them vao gio
+ * @original-sourcecode    https://businessbloomer.com/?p=21986
+ * @author        Rodolfo Melogli-Quang Truong
+ */
+ 
+add_action( 'woocommerce_before_add_to_cart_quantity', 'bbloomer_echo_qty_front_add_cart' );
+ 
+function bbloomer_echo_qty_front_add_cart() {
+ echo '<div class="qty">Số lượng: </div>'; 
+}
