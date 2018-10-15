2c3aedb8a66471ee4d5f84ff69662e03bc33b915
Quang Truong <1513772@hcmut.edu.vn>
2018-10-15 13:19:22 +0700

    Translate string in myaccount.php

---
+++ wp-content/themes/tyche/woocommerce/myaccount/dashboard.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * My Account Dashboard
+ *
+ * Shows the first intro screen on the account dashboard.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/dashboard.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @author      WooThemes
+ * @package     WooCommerce/Templates
+ * @version     2.6.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+?>
+
+<p><?php
+	/* translators: 1: user display name 2: logout url */
+	printf(
+		__( 'Hello %1$s (not %1$s? <a href="%2$s">Log out</a>)', 'woocommerce' ),
+		'<strong>' . esc_html( $current_user->display_name ) . '</strong>',
+		esc_url( wc_logout_url( wc_get_page_permalink( 'myaccount' ) ) )
+	);
+?></p>
+
+<p><?php
+	printf(
+		__( 'Từ trang tài khoản bạn có thể xem <a href="%1$s">những đơn hàng gần đây của bạn</a>, quản lý <a href="%2$s">địa chỉ giao hàng và địa chỉ thanh toán của bạn</a>, và <a href="%3$s">thay đổi mật khẩu và thông tin cá nhân</a>.', 'woocommerce' ),
+		esc_url( wc_get_endpoint_url( 'orders' ) ),
+		esc_url( wc_get_endpoint_url( 'edit-address' ) ),
+		esc_url( wc_get_endpoint_url( 'edit-account' ) )
+	);
+?></p>
+
+<?php
+	/**
+	 * My Account dashboard.
+	 *
+	 * @since 2.6.0
+	 */
+	do_action( 'woocommerce_account_dashboard' );
+
+	/**
+	 * Deprecated woocommerce_before_my_account action.
+	 *
+	 * @deprecated 2.6.0
+	 */
+	do_action( 'woocommerce_before_my_account' );
+
+	/**
+	 * Deprecated woocommerce_after_my_account action.
+	 *
+	 * @deprecated 2.6.0
+	 */
+	do_action( 'woocommerce_after_my_account' );
+
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
